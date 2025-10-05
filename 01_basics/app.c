c8b8a967d74feb93f0c7f7d3ef62a305aae489bb
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-10-05 20:35:44 +0500

    variables

--- 01_basics/app.c
+++ 01_basics/app.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 int main(){
+    // variables
+    // a variable is a container which store a value 
+    int a = 3;
+    int b= 4;
+    int sum = a+b;
+    printf("%d\n",sum);
     printf("Hello world");
+
 }
+// computer programming is a medium for us to communicate with computers
+// just like we use english or urdu to communicate with each other.
+// C is programming language.
