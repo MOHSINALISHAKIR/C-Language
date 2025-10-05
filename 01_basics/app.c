e73bb66a76ab4bc28711ec3ef3e8afa10bb13183
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-10-05 20:38:34 +0500

    input for user

--- 01_basics/app.c
+++ 01_basics/app.c
@@ -2,11 +2,17 @@
 int main(){
     // variables
     // a variable is a container which store a value 
-    int a = 3;
-    int b= 4;
-    int sum = a+b;
-    printf("%d\n",sum);
-    printf("Hello world");
+    // int a = 3;
+    // int b= 4;
+    // int sum = a+b;
+    // printf("%d\n",sum);
+    // printf("Hello world");
+
+    // input for user
+
+    int value;
+    scanf("%d",&value);
+    printf("value = %d",value);
 
 }
 // computer programming is a medium for us to communicate with computers
