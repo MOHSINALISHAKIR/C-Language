987bf8db293be3a4d3a1bcac427b42400abfdf96
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-10-08 20:46:26 +0500

    factorial do while

---
+++ 03_loops/q3do.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int fact,c,num;
+    printf("Enter Any Number For Factorial\n");
+    scanf("%d",&num);
+    fact=1;
+    c=1;
+    do
+    {
+        fact=fact*c;
+        c++;
+        /* code */
+    } while (c<=num);
+    printf("")
+    
+}
