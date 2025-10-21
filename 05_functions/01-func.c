05baeb88b5bd6894a978dee04397350f59938136
MOHSIN ALI SHAKIR <161568743+MOHSINALISHAKIR@users.noreply.github.com>
2025-10-21 06:26:04 +0500

    functions and its types

---
+++ 05_functions/01-func.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    // A function is piece of code which is written to perform a specific task it is independent of the remaining code of the program 
+    // benefits of functions
+    // easy to write a program
+    // easy to debug and test
+    // reuseability
+
+    // Types of functions
+    // Bulit in function
+    // User Defined function
+    printf("hello world");
+    
+    return 0;
+}
